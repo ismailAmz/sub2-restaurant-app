58ece0981ae82e7291b31e9337c9a75a99331cd7
ismailAmz <ismailalmuzamal1@gmail.com>
2024-06-05 10:43:37 +0700

    json, app.js dan index.html

--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -26,10 +26,10 @@ class App {
     const page = routes[url];
     this._content.innerHTML = await page.render();
     await page.afterRender();
-    const skipLinkElem = document.querySelector('.skip-link');    
-    skipLinkElem.addEventListener('click', (event) => {      
-        event.preventDefault();
-        document.querySelector('#maincontent').focus();
+    const skipLinkElem = document.querySelector('.skip-link');
+    skipLinkElem.addEventListener('click', (event) => {
+      event.preventDefault();
+      document.querySelector('#maincontent').focus();
     });
   }
 }
